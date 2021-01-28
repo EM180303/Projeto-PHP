ff56fe1bbf3ff49199238741b9ced06cf7882823
Edu Marques <e.marques180303@gmail.com>
2021-01-28 20:11:02 -0300

    carrinho

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="ept-BR">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feira em Casa</title>
 
@@ -19,7 +19,6 @@
   <link href="css/shop-homepage.css" rel="stylesheet">
 
   <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
-
 
 </head>
 
@@ -41,6 +40,18 @@
               <li class="nav-item">
                 <a class="nav-link" href="login.php">Login
                   <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.php">Carrinho
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="carrinho.php">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                  </svg>
                 </a>
               </li>
             </ul>

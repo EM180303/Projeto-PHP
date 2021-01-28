0c726fee62efadb3488f4ca727f7b3364ec85167
Edu Marques <e.marques180303@gmail.com>
2021-01-28 17:25:10 -0300

    folhagens

--- alface.php
+++ alface.php
@@ -1,3 +1,60 @@
 <?php
 require "nav.php"
 ?>
+<html lang="pt-BR">
+<head>
+   <script> type= "text/javascript"
+   function valorProduto(){
+     var total = 0;
+     var num = document.getElementById("alface").value;
+     total = 2.59 * num;
+     document.getElementById("valortotal").textContent = "Valor a ser pago: R$ " + total;
+   }
+   </script> 
+</head>
+<body>
+
+  <div class="Container">
+    <div class="row">
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+      <div class="col-sm">
+      <br>
+        <div class="card" style="width: 18rem;">
+          <img src="imagens/alface.jpg" class="card-img-top" alt="alface">
+          <div class="card-body">
+            <h5 class="card-title">ALface Lisa</h5>
+            <p class="card-text">R$ 2,59 Und.</p>
+            <p id="valortotal"> Valor a ser pago: R$ 2,59</p>
+            <p>Quantidade:</p>
+            <input class="btn btn-dark" type="number" id="alface" onblur="valorProduto();">
+            <input class="btn btn-primary" type="button" onclick="valorProduto();" value="Calcular">
+            <br>
+            <br>
+            <form action="sucesso.php">
+              <button class="btn btn-success">Adicionar ao carrinho</button>
+            </form>
+            <br>
+            <div class="row">
+              <div class="col-sm"></div>
+              <div class="col-sm">
+                <a class="btn btn-danger" href="folhagens.php">Cancelar</a>
+              </div>
+              <div class="col-sm"></div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+    </div>
+  </div>
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Feira em Casa 2021</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+</body>
+</html>
--- couve.php
+++ couve.php
@@ -1,3 +1,59 @@
 <?php
 require "nav.php"
 ?>
+<head>
+   <script> type= "text/javascript"
+   function valorProduto(){
+     var total = 0;
+     var num = document.getElementById("couve").value;
+     total = 10.99 * num;
+     document.getElementById("valortotal").textContent = "Valor a ser pago: R$ " + total;
+   }
+   </script> 
+</head>
+<body>
+
+  <div class="Container">
+    <div class="row">
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+      <div class="col-sm">
+      <br>
+        <div class="card" style="width: 18rem;">
+          <img src="imagens/couve-flor-p.jpg" class="card-img-top" alt="couve">
+          <div class="card-body">
+            <h5 class="card-title">Couve-Flor</h5>
+            <p class="card-text">R$ 10,99 Und.</p>
+            <p id="valortotal"> Valor a ser pago: R$ 10,99</p>
+            <p>Quantidade:</p>
+            <input class="btn btn-dark" type="number" id="couve" onblur="valorProduto();">
+            <input class="btn btn-primary" type="button" onclick="valorProduto();" value="Calcular">
+            <br>
+            <br>
+            <form action="sucesso.php">
+              <button class="btn btn-success">Adicionar ao carrinho</button>
+            </form>
+            <br>
+            <div class="row">
+              <div class="col-sm"></div>
+              <div class="col-sm">
+                <a class="btn btn-danger" href="folhagens.php">Cancelar</a>
+              </div>
+              <div class="col-sm"></div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+    </div>
+  </div>
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Feira em Casa 2021</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+</body>
+</html>

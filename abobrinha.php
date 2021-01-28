1eb58cdb13769fbea61e6f728ce954e69c7f761e
Edu Marques <e.marques180303@gmail.com>
2021-01-28 16:52:09 -0300

    verduras

--- abobrinha.php
+++ abobrinha.php
@@ -1,3 +1,60 @@
 <?php
 require "nav.php"
 ?>
+<html lang="pt-BR">
+<head>
+   <script> type= "text/javascript"
+   function valorProduto(){
+     var total = 0;
+     var num = document.getElementById("abobrinha").value;
+     total = 4.80 * num;
+     document.getElementById("valortotal").textContent = "Valor a ser pago: R$ " + total;
+   }
+   </script> 
+</head>
+<body>
+
+  <div class="Container">
+    <div class="row">
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+      <div class="col-sm">
+      <br>
+        <div class="card" style="width: 18rem;">
+          <img src="imagens/abobrinha-2-p.jpg" class="card-img-top" alt="abobrinha">
+          <div class="card-body">
+            <h5 class="card-title">Abobrinha</h5>
+            <p class="card-text">R$ 4,80 Kg</p>
+            <p id="valortotal"> Valor a ser pago: R$ 4,80</p>
+            <p>Quantidade:</p>
+            <input class="btn btn-dark" type="number" id="abobrinha" onblur="valorProduto();">
+            <input class="btn btn-primary" type="button" onclick="valorProduto();" value="Calcular">
+            <br>
+            <br>
+            <form action="sucesso.php">
+              <button class="btn btn-success">Adicionar ao carrinho</button>
+            </form>
+            <br>
+            <div class="row">
+              <div class="col-sm"></div>
+              <div class="col-sm">
+                <a class="btn btn-danger" href="verduras.php">Cancelar</a>
+              </div>
+              <div class="col-sm"></div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+    </div>
+  </div>
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Feira em Casa 2021</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+</body>
+</html>
--- tomate.php
+++ tomate.php
@@ -1,3 +1,60 @@
 <?php
 require "nav.php"
 ?>
+<html lang="pt-BR">
+<head>
+   <script> type= "text/javascript"
+   function valorProduto(){
+     var total = 0;
+     var num = document.getElementById("tomate").value;
+     total = 20.99 * num;
+     document.getElementById("valortotal").textContent = "Valor a ser pago: R$ " + total;
+   }
+   </script> 
+</head>
+<body>
+
+  <div class="Container">
+    <div class="row">
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+      <div class="col-sm">
+      <br>
+        <div class="card" style="width: 18rem;">
+          <img src="imagens/tomate-p.jpg" class="card-img-top" alt="tomate">
+          <div class="card-body">
+            <h5 class="card-title">Tomate Cereja</h5>
+            <p class="card-text">R$ 20,99 Kg</p>
+            <p id="valortotal"> Valor a ser pago: R$ 20,99</p>
+            <p>Quantidade:</p>
+            <input class="btn btn-dark" type="number" id="tomate" onblur="valorProduto();">
+            <input class="btn btn-primary" type="button" onclick="valorProduto();" value="Calcular">
+            <br>
+            <br>
+            <form action="sucesso.php">
+              <button class="btn btn-success">Adicionar ao carrinho</button>
+            </form>
+            <br>
+            <div class="row">
+              <div class="col-sm"></div>
+              <div class="col-sm">
+                <a class="btn btn-danger" href="verduras.php">Cancelar</a>
+              </div>
+              <div class="col-sm"></div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div class="col-sm"></div>
+      <div class="col-sm"></div>
+    </div>
+  </div>
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Feira em Casa 2021</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+</body>
+</html>

7bfab025303bf41f6831c77246d009c29b706907
Edu Marques <e.marques180303@gmail.com>
2021-02-04 11:44:16 -0300

    login bd

--- v_login.php
+++ v_login.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+/*
 $usuario_autenticado = false;
 
 $usuarios = array();
@@ -31,3 +32,29 @@ if($usuario_autenticado){
     $_SESSION['validado'] = 'NÃO';
     header('Location:login.php?login=erro');
 }
+*/
+print_r($_POST);
+print_r($_SESSION);
+header('Access-Control-Allow-Origin: *');
+require("conexao.php");
+$email = $_POST['email'];
+$senha = $_POST['pass'];
+$query_ = "SELECT * FROM usuarios WHERE us_email = '$email' AND us_senha = '$senha'";
+$resultado = $conect->query($query_);
+
+if($resultado->num_rows > 0){
+    while($row = $resultado->fetch_assoc()){
+        $_SESSION['idusuario'] = $row['us_id'];
+        $_SESSION['nome'] = $row['us_nome'];
+        $_SESSION['validado'] = 'SIM';
+        echo ("<script>
+          window.alert('Login realizado com Sucesso!')
+          window.location.href='index.php';
+      </script>");
+        #header('Location:index.php');
+    }
+} else{
+    $_SESSION['validado'] = 'NÃO';
+    header('Location:index.php?login=erro1');
+}
+$conect->close();

52b1a94e694e578a27118d3dcc7869da87a1dfd6
Edu Marques <e.marques180303@gmail.com>
2021-02-11 20:24:19 -0300

    correção

--- finalizar.php
+++ finalizar.php
@@ -4,28 +4,32 @@ header('Location:carrinho.php?cart=ok');
 require "valid_navegacao.php";
 require "conexao.php";
 
+$total = 0;
+#if (isset ($_GET['cart']) && $_GET['cart'] == 'ok'){
+    foreach ($_SESSION['carrinho'] as $key => $value) :
+        $pg = ($value['valor'] * $value['quantidade']);
+        $total = $pg + $total;
 
-if (isset ($_GET['cart']) && $_GET['cart'] == 'ok');
-foreach ($_SESSION['carrinho'] as $key => $value) :
-    $pg = ($value['valor'] * $value['quantidade']);
-    $total = $pg + $total;
+        $us_id = $_SESSION['idusuario'];
 
-    $us_id = $_SESSION['idusuario'];
+        $data = date('ymd');
+        
+        $produto = $value['produto'];
+        $quantidade = $value['quantidade'];
+        $valor = $value['valor'];
+        $valor_pg = $valor * $quantidade;
 
-    $data = date('ymd');
-    
-    $produto = $value['produto'];
-    $quantidade = $value['quantidade'];
-    $valor = $value['valor'];
-    $valor_pg = $valor * $quantidade;
+        print_r($_SESSION['carrinho'], $us_id);
+        print_r($data, $valor_pg);
 
-    $stmt = $conect ->prepare("INSERT INTO carrinho (ce_ca_id,ca_produto,ca_quantidade,ca_valor,ca_valor_pg,ca_data) VALUES (?,?,?,?,?,?)");
-    $stmt->bind_param("isissd", $us_id, $produto, $quantidade, $valor, $valor_pg, $data);
-    $stmt->execute();
-    $conect->close();
+        $stmt = $conect ->prepare("INSERT INTO carrinho (ce_ca_id,ca_produto,ca_quantidade,ca_valor,ca_valor_pg,ca_data) VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param("isissd", $us_id, $produto, $quantidade, $valor, $valor_pg, $data);
+        $stmt->execute();
+        
 
-
- endforeach;
+    endforeach;
+#}
+$conect->close();
  
 echo'Valor total da compra: R$ '.$total.''; 
 
